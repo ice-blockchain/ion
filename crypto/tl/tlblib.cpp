05aea3c9dc3378ed474051ee9c3ed533256cf475
Victor Tec <victor@ton.org>
2025-10-23 19:01:47 +0300

    Add utf8 check for string in JsonPrinter

--- crypto/tl/tlblib.cpp
+++ crypto/tl/tlblib.cpp
@@ -17,6 +17,7 @@
     Copyright 2017-2020 Telegram Systems LLP
 */
 #include "td/utils/base64.h"
+#include "td/utils/utf8.h"
 #include "vm/cells/Cell.h"
 #include "vm/cells/CellBuilder.h"
 #include "tl/tlblib.hpp"
@@ -302,7 +303,11 @@ bool SnakeString::print_skip(JsonPrinter& pp, vm::CellSlice& cs) const {
   if (text_result.is_error()) {
     return pp.fail(text_result.error().message().str());
   }
-  return pp.out(text_result.move_as_ok());
+  auto str = text_result.move_as_ok();
+  if (!td::check_utf8(str)) {
+    return pp.fail("invalid utf-8 string");
+  }
+  return pp.out(str);
 }
 
 bool TupleT::skip(vm::CellSlice& cs) const {
