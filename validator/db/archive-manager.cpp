8be5ea5e44ccd8a4e454f791e0aa07c4dbb7315d
Marat S <dungeon666master@protonmail.com>
2025-12-11 05:06:24 +0000

    catch up last temp slice

--- validator/db/archive-manager.cpp
+++ validator/db/archive-manager.cpp
@@ -1093,6 +1093,17 @@ void ArchiveManager::try_catch_up_with_primary(td::Promise<td::Unit> promise) {
         return;
       }
     }
+
+    // catch up last temp package to sync latest blocks
+    if (d == x->temp_packages_.back()) {
+      auto P = td::PromiseCreator::lambda([id, promise = ig.get_promise()](td::Result<td::Unit> R) mutable {
+        if (R.is_error()) {
+          LOG(ERROR) << "Failed to catch up last archive slice " << id.path();
+        }
+        promise.set_result(std::move(R));
+      });
+      td::actor::send_closure(get_file_map(id).find(id)->second.file_actor_id(), &ArchiveSlice::try_catch_up_with_primary, std::move(P));
+    }
   }
   promise2.set_value(td::Unit());
 }
